3522abe698ca7296196aefcc9b453e0bf3097ffc
ps4515 <simatosp@gmail.com>
2018-06-13 16:54:30 +0100

    changes keystones -> classes

--- keystones.js
+++ keystones.js
@@ -1,151 +1,109 @@
 var keystones = [
 	{
 		"id": 0,
-		"name": "Acrobatics", 
-		"description": "30% Chance to Dodge Attacks.\n50% less Armour and Energy Shield, 30% less Chance to Block Spells and Attacks"
+		"name": "Juggernaut",
+		"description": "T H I C C  J U G G"
 	},
-	
+
 	{
 		"id": 1,
-		"name": "Ancestral Bond", 
-		"description": "Can have up to 1 additional Totem summoned at a time\nYou can't deal Damage with your Skills yourself"
+		"name": "Berserker",
+		"description": "Marauder"
 	},
-	
+
 	{
 		"id": 2,
-		"name": "Avatar of Fire",
-	    "description": "50% of Physical, Cold and Lightning Damage Converted to Fire Damage\nDeal no Non-Fire Damage"
+		"name": "Chieftain",
+		"description": "Marauder"
 	},
-	
+
 	{
 		"id": 3,
-		"name": "Blood Magic", 
-		"description": "Removes all mana.\nSpend Life instead of Mana for Skills"
+		"name": "Slayer",
+		"description": "Duelist"
 	},
-	
+
 	{
 		"id": 4,
-		"name": "Chaos Inoculation", 
-		"description": "Maximum Life becomes 1\nImmune to Chaos Damage"
+		"name": "Gladiator",
+		"description": "Duelist"
 	},
-	
+
 	{
 		"id": 5,
-		"name": "Conduit", 
-		"description": "Share Endurance, Frenzy and Power Charges with nearby party members"
+		"name": "Champion",
+		"description": "Duelist"
 	},
-	
+
 	{
 		"id": 6,
-		"name": "Eldritch Battery", 
-		"description": "Energy Shield protects Mana instead of Life\nSpend Energy Shield before Mana for Skill Costs"
+		"name": "Deadeye",
+		"description": "Ranger"
 	},
-	
+
 	{
 		"id": 7,
-		"name": "Elemental Equilibrium", 
-		"description": "Enemies you hit with Elemental Damage temporarily get +25% Resistance to those Elements and -50% Resistance to other Elements"
+		"name": "Raider",
+		"description": "Ranger"
 	},
-	
+
 	{
 		"id": 8,
-		"name": "Ghost Reaver", 
-		"description": "Life Leech is applied to Energy Shield instead\n50% less Energy Shield Recharge Rate"
+		"name": "Pathfinder",
+		"description": "Ranger"
 	},
-	
+
 	{
 		"id": 9,
-		"name": "Iron Grip", 
-		"description": "The increase to Physical Damage from Strength applies to Projectile Attacks as well as Melee Attacks"
+		"name": "Assassin",
+		"description": "Shadow"
 	},
-	
+
 	{
 		"id": 10,
-		"name": "Iron Reflexes", 
-		"description": "Converts all Evasion Rating to Armour.\nDexterity provides no bonus to Evasion Rating"
+		"name": "Saboteur",
+		"description": "Shadow"
 	},
-	
+
 	{
 		"id": 11,
-		"name": "Mind Over Matter",
-		 "description": "30% of Damage is taken from Mana before Life"
+		"name": "Trickster",
+		 "description": "Shadow"
 	},
-	
+
 	{
 		"id": 12,
-		"name": "Minion Instability",
-		"description": "Minions explode when reduced to low life, dealing 33% of their maximum life as fire damage to surrounding enemies"
+		"name": "Necromancer",
+		"description": "Witch"
 	},
-	
+
 	{
 		"id": 13,
-		"name": "Necromantic Aegis", 
-		"description": "All bonuses from an equipped Shield apply to your Minions instead of you"
+		"name": "Occultist",
+		"description": "Witch"
 	},
-	
+
 	{
 		"id": 14,
-		"name": "Pain Attunement", 
-		"description": "30% more Spell Damage when on Low Life"
+		"name": "Elementalist",
+		"description": "Witch"
 	},
-	
+
 	{
 		"id": 15,
-		"name": "Phase Acrobatics", 
-		"description": "30% Chance to Dodge Spell Damage"
+		"name": "Inquisitor",
+		"description": "Templar"
 	},
-	
+
 	{
 		"id": 16,
-		"name": "Point Blank", 
-		"description": "Projectile Attacks deal up to 50% more Damage to targets at the start of their movement, dealing less Damage to targets as the projectile travels farther"
+		"name": "Hierophant",
+		"description": "Templar"
 	},
-	
+
 	{
 		"id": 17,
-		"name": "Resolute Technique", 
-		"description": "Your hits can't be Evaded\nNever deal Critical Strikes"
-	},
-	
-	{
-		"id": 18,
-		"name": "Unwavering Stance", 
-		"description": "Cannot Evade enemy Attacks\nCannot be Stunned"
-	},
-	
-	{
-		"id": 19,
-		"name": "Vaal Pact", 
-		"description": "Gain Life from Leech instantly.\nLife Regeneration has no effect"
-	},
-	
-	{
-		"id": 20,
-		"name": "Zealot's Oath", 
-		"description": "Life Regeneration applies to Energy Shield instead of Life"
-	},
-
-	{
-		"id": 21,
-		"name": "Arrow Dancing",
-		"description": "40% more chance to Evade Projectile Attacks\n20% less chance to Evade Melee Attacks"
-	},
-
-	{
-		"id": 22,
-		"name": "Crimson Dance",
-		"description": "You can inflict Bleeding on an Enemy up to 8 times\nYour Bleeding does not deal extra Damage while the Enemy is moving\n50% less Damage with Bleeding"
-	},
-
-	{
-		"id": 23,
-		"name": "Elemental Overload",
-		"description": "40% more Elemental Damage if you've Crit in the past 8 seconds\nNo Critical Strike Multiplier"
-	},
-
-	{
-		"id": 24,
-		"name": "Perfect Agony",
-		"description": "Modifiers to Critical Strike Multiplier also apply to Damage Multiplier for Ailments from Critical Strike at 30% of their value\n30% less Damage with Hits"
+		"name": "Guardian",
+		"description": "Templar"
 	}
 ];
